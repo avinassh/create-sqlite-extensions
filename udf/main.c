dac03c6713850f4dd9f2ee1de0d74e9791431e88
Avinash Sajjanshetty <opensource@avi.im>
2024-03-09 11:00:34 +0530

    clang fmt and rename fn to greet

--- udf/main.c
+++ udf/main.c
@@ -1,11 +1,11 @@
 #include "sqlite3.h"
 
-#include <stdio.h>
 #include <assert.h>
+#include <stdio.h>
 
-static void hello_world(sqlite3_context *context, int argc, sqlite3_value **argv) {
+static void greet(sqlite3_context *context, int argc, sqlite3_value **argv) {
   assert(argc == 1);
-  const char *input = (const char *) sqlite3_value_text(argv[0]);
+  const char *input = (const char *)sqlite3_value_text(argv[0]);
   char *result = sqlite3_mprintf("Hello, %s!", input);
   sqlite3_result_text(context, result, -1, SQLITE_TRANSIENT);
   sqlite3_free(result);
@@ -21,15 +21,17 @@ int main() {
   }
 
   // register our hello function
-  rc = sqlite3_create_function(db, "hello", 1, SQLITE_UTF8, NULL, hello_world, NULL, NULL);
+  rc = sqlite3_create_function(db, "greet", 1, SQLITE_UTF8, NULL, greet, NULL,
+                               NULL);
   if (rc != SQLITE_OK) {
-    fprintf(stderr, "failed to register hello function: %s\n", sqlite3_errmsg(db));
+    fprintf(stderr, "failed to register hello function: %s\n",
+            sqlite3_errmsg(db));
     sqlite3_close(db);
     return 1;
   }
   // run a query that calls our hello function
   sqlite3_stmt *stmt;
-  rc = sqlite3_prepare_v2(db, "SELECT hello(\"v\");", -1, &stmt, NULL);
+  rc = sqlite3_prepare_v2(db, "SELECT greet(\"v\");", -1, &stmt, NULL);
   if (rc != SQLITE_OK) {
     fprintf(stderr, "failed to prepare query: %s\n", sqlite3_errmsg(db));
     sqlite3_close(db);
