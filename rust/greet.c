ce92eee3e4d91c604eed70f68e8110de9784c7ae
Avinash Sajjanshetty <opensource@avi.im>
2024-03-09 18:39:51 +0530

    Port the extenstion to rust

--- rust/greet.c
+++ rust/greet.c
@@ -4,17 +4,7 @@ SQLITE_EXTENSION_INIT1
 #include <assert.h>
 #include <stdio.h>
 
-extern char *greet(const char *str);
-extern void greet_free(char *);
-
-static void greet_wrapper(sqlite3_context *context, int argc,
-                          sqlite3_value **argv) {
-  assert(argc == 1);
-  const char *input = (const char *)sqlite3_value_text(argv[0]);
-  char *result = greet(input);
-  sqlite3_result_text(context, result, -1, SQLITE_TRANSIENT);
-  greet_free(result);
-}
+extern void greet(sqlite3_context *context, int argc, sqlite3_value **argv);
 
 #ifdef _WIN32
 __declspec(dllexport)
@@ -27,8 +17,8 @@ int sqlite3_greet_init(
   int rc = SQLITE_OK;
   SQLITE_EXTENSION_INIT2(pApi);
 
-  rc = sqlite3_create_function(db, "greet", 1, SQLITE_UTF8, NULL, greet_wrapper,
-                               NULL, NULL);
+  rc = sqlite3_create_function(db, "greet", 1, SQLITE_UTF8, NULL, greet, NULL,
+                               NULL);
   if (rc != SQLITE_OK) {
     fprintf(stderr, "error creating function: %s\n", sqlite3_errmsg(db));
     return rc;
--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,29 +1,38 @@
 extern crate libc;
 
-use libc::c_char;
 use std::ffi::{CStr, CString};
+use libsql_ffi::{sqlite3_context, sqlite3_free, sqlite3_int64, sqlite3_result_text, SQLITE3_TEXT, sqlite3_value, sqlite3_value_text, sqlite3_value_type, SQLITE_TRANSIENT};
+
 
 #[no_mangle]
-pub extern "C" fn greet(s: *const c_char) -> *mut c_char {
-    let c_str = unsafe {
-        assert!(!s.is_null());
-        CStr::from_ptr(s)
-    };
-    println!("I am in rust");
-    let r_str = c_str.to_str().unwrap();
-    let c_str_song = CString::new(format!("hello, {r_str}!")).unwrap();
-    c_str_song.into_raw()
-}
-
-#[no_mangle]
-pub extern "C" fn greet_free(s: *mut c_char) {
+pub extern "C" fn greet(context: *mut sqlite3_context, argc: sqlite3_int64, argv: *const *mut sqlite3_value) {
+    assert_eq!(argc, 1);
     unsafe {
-        if s.is_null() {
+        if argv.is_null() {
             return;
         }
-        drop(CString::from_raw(s))
-    };
+        let param = *argv.offset(0);
+        if param.is_null() {
+            // error out
+        }
+        if sqlite3_value_type(param) != SQLITE3_TEXT {
+            // error out
+        }
+        let c_char = sqlite3_value_text(param);
+        if c_char.is_null() {
+            // error out
+        }
+        let input_str = CStr::from_ptr(c_char as *const std::os::raw::c_char);
+        let input = input_str.to_str().unwrap();
+
+        let result = format!("Hellooo, {}!", input);
+        let c_result = CString::new(result.as_str()).unwrap();
+        let c_result_ptr = c_result.into_raw();
+        sqlite3_result_text(context, c_result_ptr, -1, SQLITE_TRANSIENT());
+        sqlite3_free(c_result_ptr as *mut std::ffi::c_void);
+    }
 }
+
 
 pub fn add(left: usize, right: usize) -> usize {
     left + right
