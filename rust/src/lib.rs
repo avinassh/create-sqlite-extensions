f3e6641d87f7f3ddcf33cc0e43e7ccae8aa5b463
Avinash Sajjanshetty <opensource@avi.im>
2024-03-13 16:34:11 +0530

    simplify lib for debugging

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,35 +1,18 @@
 extern crate libc;
 
 use std::ffi::{CStr, CString};
-use libsql_ffi::{sqlite3_context, sqlite3_free, sqlite3_int64, sqlite3_result_text, SQLITE3_TEXT, sqlite3_value, sqlite3_value_text, sqlite3_value_type, SQLITE_TRANSIENT};
+use std::os::raw::{c_char, c_void};
+use libsql_ffi::{sqlite3_context, sqlite3_free, sqlite3_int64, sqlite3_result_text, SQLITE3_TEXT, sqlite3_value, sqlite3_value_text, sqlite3_value_type, SQLITE_STATIC, SQLITE_TRANSIENT};
 
 
 #[no_mangle]
 pub extern "C" fn greet(context: *mut sqlite3_context, argc: sqlite3_int64, argv: *const *mut sqlite3_value) {
     assert_eq!(argc, 1);
     unsafe {
-        if argv.is_null() {
-            return;
-        }
-        let param = *argv.offset(0);
-        if param.is_null() {
-            // error out
-        }
-        if sqlite3_value_type(param) != SQLITE3_TEXT {
-            // error out
-        }
-        let c_char = sqlite3_value_text(param);
-        if c_char.is_null() {
-            // error out
-        }
-        let input_str = CStr::from_ptr(c_char as *const std::os::raw::c_char);
-        let input = input_str.to_str().unwrap();
-
-        let result = format!("Hellooo, {}!", input);
-        let c_result = CString::new(result.as_str()).unwrap();
-        let c_result_ptr = c_result.into_raw();
-        sqlite3_result_text(context, c_result_ptr, -1, SQLITE_TRANSIENT());
-        sqlite3_free(c_result_ptr as *mut std::ffi::c_void);
+        let rust_string = "Hellooooo!, World!";
+        let c_string = CString::new(rust_string).unwrap();
+        let c_string_raw = c_string.into_raw();
+        sqlite3_result_text(context, c_string_raw, -1, SQLITE_TRANSIENT());
     }
 }
 
